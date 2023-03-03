6a56145956f51cde92144226799ac2c98597208e
OuChiaYun <j312079@gmail.com>
2023-03-03 23:26:15 +0800

    new code

--- Source/Game/mygame_run.cpp
+++ Source/Game/mygame_run.cpp
@@ -75,7 +75,7 @@ void CGameStateRun::OnInit()  								// 遊戲的初值及圖形設定
 	});
 	background.SetTopLeft(0, 0);
 
-	character.LoadBitmapByString({ "resources/witch.bmp" });
+	character.LoadBitmapByString({ "resources/giraffe.bmp" });
 	character.SetTopLeft(150, 265);
 
 	chest_and_key.LoadBitmapByString({ "resources/chest.bmp", "resources/chest_ignore.bmp" }, RGB(255, 255, 255));
